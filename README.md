c9811452fd8eccaed8ee695bf183d514eeebc8b7
LucasLeRay <contact@lucas-le-ray.com>
2020-11-10 17:45:43 +0100

    feat: init lambda

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # serverless-graphql-api-boilerplate
+
 A Serverless API boilerplate using AWS Lambda, GraphQL and Typescript
---
+++ src/graphql.ts
@@ -0,0 +1,21 @@
+import { ApolloServer, gql } from 'apollo-server-lambda'
+
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`
+
+const resolvers = {
+  Query: {
+    hello: () => 'Hello world!',
+  },
+}
+
+const server = new ApolloServer({ typeDefs, resolvers })
+
+export const handler = server.createHandler({
+  cors: {
+    origin: '*',
+  },
+})
